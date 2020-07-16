60a6832848059e7b259003bddae5dccc4ac2d120
Toiwilou Hassane <toiwilouhassane@gmail.com>
2020-07-16 11:57:43 +0200

    test

---
+++ precache-manifest.ca394d98de7d27e8ed3121fdab506253.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df224a25f784c01473737890a512f1be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d649313731d034bfe13e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "489b6faa09a8875f1020",
+    "url": "/static/js/2.f87ff9e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f87ff9e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d649313731d034bfe13e",
+    "url": "/static/js/main.22656d74.chunk.js"
+  },
+  {
+    "revision": "63797f236d2fdfa8d410",
+    "url": "/static/js/runtime-main.cc21c881.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
